8a9f4af70b3899cf73f1242f1eacf5ce315bb1ee
ketandabre22 <ketandabre99per@gmail.com>
2025-04-20 13:33:24 +1000

    Initial commit: Added Main.py

---
+++ Main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import messagebox
+import os
+import base64
+
+def main_screen():
+
+      screen =  Tk()
+      screen.geometry("375x398")
+
+      screen.title("PctApp")
+
+      screen.mainloop()
+
+main_screen()
